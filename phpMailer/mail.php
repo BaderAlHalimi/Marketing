3d5d7f65780d9d6fc2f3765b4d10d0d561918572
BaderAlHalimi <belhalimi@smail.ucas.edu.ps>
2023-01-30 20:41:18 +0200

    Initial commit

--- phpMailer/mail.php
+++ phpMailer/mail.php
@@ -16,8 +16,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'badermarket7@gmail.com';                     //SMTP username
-$mail->Password   = 'xlgpyaofvcygopbj';                               //SMTP password
+$mail->Username   = 'badermarket0@gmail.com';                     //SMTP username
+$mail->Password   = 'fzyympbrqtvwfvla';                               //SMTP password
 $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
 $mail->Port       = 465;  
 
